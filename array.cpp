4657e3d890691ae3b9d7888c1f433d95e07d6403
AmnaArifH <108061760+AmnaArifH@users.noreply.github.com>
2022-07-21 17:58:03 +0500

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -1,70 +1,29 @@
+//SHIFTING ARRAY ELEMENTS ON LEFT SIDE............
+
 #include <iostream>
 using namespace std;
+#define n 5
 int main()
 {
-//	int marks[]={44,66,88,99,46};
-//	cout<<"marks [0] :"<<marks[0]<<endl;
-//	cout<<"address of 0 index: "<<&marks[0]<<endl;
-//	int *p;
-//	p=marks;
-//	cout<<"address of 0 index: "<<p<<endl;
-//	cout<<"value of 0 index: "<<*p<<endl;
-//	cout<<"marks [2] :"<<marks[2]<<endl;
-
-
-//int marks[5];
-//for(int i=0;i<5;i++)
-//{
-//	cout<<"ENTER MARKS ["<<i<<"]:";
-//	cin>>marks[i];
-//}
-//cout<<"ALL THE MARKS YOU HAVE ENTERED: "<<endl;
-//for(int i=0;i<5;i++)
-//{
-//	cout<<marks[i]<<" ";
-//}
-
-
-//int marks[]={10,20,30,40,00};
-//int sum=0;
-//int n=5;
-//for(int i=0;i<n;i++)
-//{
-//	sum=sum+marks[i];
-//}
-//cout<<"sum is : "<<sum;
-
-
-int marks[50];
-int i;
-int size;
-cout<<"ENTER SIZE OF ARRAY  YOU WNAT TO ENTER :";
-cin>>size;
-int p;
-cout<<"ENTER POSITION:";
-cin>>p;
-cout<<"ENTER "<<size<<" ELEMENTS FOR ARRAY:"<<endl;
-for(int i=0;i<size;i++)
-{
-	cin>>marks[i];
-}
-cout<<"ALL ELEMENTS ARE:"<<endl;
-for(int i=0;i<size;i++)
-{
-	cout<<marks[i]<<" ";
-}
-int ele;
-cout<<"ENTER ELEMENT:"<<endl;
-cin>>ele;
-cout<<"\n";
-for(int i=size;i>=p;i--)
-{
-	marks[i]=marks[i-1];
-}
-marks[i]=ele;
-cout<<"AFTER CHANGING: "<<endl;
-for(int i=0;i<size;i++)
-{
-	cout<<marks[i]<<" ";
-}
+	// shift array elements to left side...
+	int arr[n],i;
+	int temp;
+	cout<<"ENTER AN ARRAY..."<<endl;
+	for(i=0;i<5;i++)
+	{
+		cin>>arr[i];
+	}
+	
+	temp=arr[0];
+   	for(i=0;i<5;i++)
+	{
+	arr[i]=arr[i+1];
+	arr[n-1]=temp;
+	}
+	
+	cout<<" ARRAY AFTER LEFT SHIFT..."<<endl;
+	for(i=0;i<5;i++)
+	{
+		cout<<arr[i]<<"\t";
+	}
 }
